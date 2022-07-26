29350a5435dc0d271d190cf83a6d7777431876dc
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-26 15:57:53 +0530

    Add files via upload

---
+++ functions_1.py
@@ -0,0 +1,36 @@
+''' functions in python 
+ In functions we need to call the function '''
+
+# def student():
+#     print('I am a student')
+# student()                                              # I am a student                         
+
+
+# def user(n):
+#     print('user not matching')
+#     user(5)
+# user(10)                                            # RecursionError: maximum recursion depth exceeded while calling a Python object
+
+
+# def user(n):
+#     print('user not matching')
+# user(10)                                        # user not matching
+
+
+
+# def hi(fname,lname):
+#     print('My first name is ',fname,'My last name is ',lname)
+# hi('Mohammad','shahed')                         # My first name is  Mohammad My last name is  shahed
+
+
+
+# def word(kilo):
+#     print('Email already exists '+str(kilo))
+# word(256)                                          # Email already exists 256            
+# word('Try to use another Email')                   # Email already exists Try to use another Email
+
+
+# def compile(*code):
+#     print('I am  executing all codes ', *code ,sep=',')
+# compile(*('java','c','c++','python','django.'))       # I am  executing all codes ,java,c,c++,python,django.
+# compile(*['MYSQl','POSTGRESQL','Dmongodb','Oracle'])    # I am  executing all codes ,MYSQl,POSTGRESQL,Dmongodb,Oracle
