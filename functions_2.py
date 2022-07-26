e130deabe0c1f4f0531c40d99507608a36a7c9a6
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-26 15:58:31 +0530

    Add files via upload

---
+++ functions_2.py
@@ -0,0 +1,53 @@
+# def grow():
+#     print('Coding environment')
+# grow()                                              # Coding environment
+
+
+# def get(dew):
+#     print('I am the '+dew)
+# get('a programer')                                      # I am the a programer
+# get('a developer')                                      # I am the a developer
+
+
+# def student(*kim):
+#     print("today is my "+kim[2])                # This 2 indicates index of the student paramerter to insert in it.
+# student('Birthday','deathday','blastday','colorfulday')  # today is my blastday
+
+
+# def uber(n,a):
+#     print('Hey '+n+'! your age is '+str(a))                  # we can keep the values in str or we can convert the variable into str
+# uber('Mohd','26')                                       #  Hey Mohd! your age is 26
+# uber(n='abdul',a='24')                                  # Hey abdul! your age is 24
+# uber(a=19,n='sam')                                      # Hey sam! your age is 19
+
+
+# def crow(m,k):
+#     print('Hey '+m+' you are in '+k)
+# m = input("Enter the value of m :")
+# k = (input("Enter the value of k :"))
+# crow(m,k)                                               # Hey programmer you are in 2021
+
+
+# def operations(r, y):
+#     return r+y
+# print(operations(10, 20))                         # 30
+
+
+# def operations(r, y):
+#     print(r+y)
+# operations(15,26.3)                                 # 41.3
+
+
+
+# def dell(a,c):
+#     return a+c, a**c;
+# a = int(input("Enter the value of a : "))
+# c = float(input("Enter the value of c :"))
+# print(dell(a,c))                                        # (5.0, 9.0)
+
+
+# def gel(d,j):
+#     print(d-j,j**d);
+# d = int(input("Enter the value of d :"))
+# j = int(input("Enter the value of j :"))
+# gel(d,j)                                                 # 20 298023223876953125
